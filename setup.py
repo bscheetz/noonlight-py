96a46f98d4cbe9dc50d179d382a8f7e5fcdc5f0b
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2025-01-06 21:32:40 -0500

    Add services type of "other".
    Also bump the version since the functionality has changed.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 import os
 import sys
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
     sys.exit()
 
 
@@ -15,19 +15,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='noonlight',
-    version='0.1.1',
-    packages=['noonlight'],
-    url='https://github.com/konnected-io/noonlight-py',
+    name="noonlight",
+    version="0.1.2",
+    packages=["noonlight"],
+    url="https://github.com/konnected-io/noonlight-py",
     license="MIT License",
-    description='A Python library for interacting with the Noonlight API',
+    description="A Python library for interacting with the Noonlight API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author='Nate Clark, Nick Gordon, Konnected Inc',
-    author_email='help@konnected.io',
-    install_requires=['aiohttp'],
+    author="Nate Clark, Nick Gordon, Konnected Inc",
+    author_email="help@konnected.io",
+    install_requires=["aiohttp"],
     project_urls={
-      'Homepage': 'https://github.com/konnected-io/noonlight-py',
-      'API Documentation': 'https://docs.noonlight.com'
-    }
+        "Homepage": "https://github.com/konnected-io/noonlight-py",
+        "API Documentation": "https://docs.noonlight.com",
+    },
 )
